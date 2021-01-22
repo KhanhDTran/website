2f2441b0a2c3d18b64d6c00322c2d49470f9a1cb
KhanhDTran <75887922+KhanhDTran@users.noreply.github.com>
2021-01-22 19:47:32 +0700

    1

---
+++ app.js
@@ -0,0 +1,50 @@
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+        navLinks.forEach(link=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = 'navLinkFade 0.5s ease forwards 0.5s';
+            }
+
+        })
+        navLinks.forEach(link=>{
+            link.style.fontSize = '25px';
+        })
+        burger.classList.toggle('toggle');
+    })
+    
+}
+
+navSlide();
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var modalEle = document.querySelector(".modal");
+var modalImage = document.querySelector(".modalImage");
+Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+   item.addEventListener("click", event => {
+      modalEle.style.display = "block";
+      modalImage.src = event.target.src;
+   });
+});
+document.querySelector(".close").addEventListener("click", () => {
+   modalEle.style.display = "none";
+});
